dea93337245e91d6a28c8286a4ab8aef37bbaf57
Kurniadinurmiati <43645904+Kurniadinurmiati@users.noreply.github.com>
2018-10-31 07:25:03 -0700

    Create ArrayListDouble.java

---
+++ ArrayListDouble.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+public class ArrayListDouble {
+//Nama : kur
+//Kelas : TeknikInformatikaE 17
+		public static void main(Double[] args) {
+			ArrayList<Double> kur = new ArrayList<Double>();
+			kur.add(0.34533D);
+			kur.add(0.35455D);
+			kur.add(0.133434D);
+			kur.add(0.9232325D);
+			System.out.println(kur.toString());
+			System.out.println("INDEX NYA A: "+kur.indexOf(0.23D));
+			System.out.println("Size: "+kur.size());
+	}
+
+	}
+
