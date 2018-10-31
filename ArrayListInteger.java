d377445a2836662921476c002ff2071e17608fcb
Kurniadinurmiati <43645904+Kurniadinurmiati@users.noreply.github.com>
2018-10-31 07:15:32 -0700

    Create ArrayListInteger.java

---
+++ ArrayListInteger.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class ArrayListInteger {
+	//Nama : kur
+	//Kelas : TeknikInformatikaE 17
+	public static void main(int[] args) {
+		ArrayList<Integer> kur = new ArrayList<Integer>();
+		kur.add(9);
+		kur.add(4);
+		kur.add(6);
+		System.out.println( kur.toString());
+		System.out.println("INDEX NYA A: "+ kur.indexOf(1));
+		System.out.println("Size: "+ kur.size());
+}
+
+}
