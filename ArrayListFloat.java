a7281540de286a15aa45b4aa38323c3bdbd46b6d
Kurniadinurmiati <43645904+Kurniadinurmiati@users.noreply.github.com>
2018-10-31 07:19:29 -0700

    Update ArrayListFloat.java

--- ArrayListFloat.java
+++ ArrayListFloat.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 public class ArrayListFloat {
-	//Nama : Paisal
+	//Nama : kur
 	//Kelas : TeknikInformatikaE 17
 	public static void main(Float[] args) {
 		ArrayList<Float> Paisal = new ArrayList<Float>();
-		Paisal.add(7.3F);
-		Paisal.add(9.5F);
-		Paisal.add(2.9F);
-		Paisal.add(5.9F);
+		kur.add(7.3F);
+		kur.add(9.5F);
+		kur.add(2.9F);
+		kur.add(5.9F);
 		System.out.println(Paisal.toString());
-		System.out.println("INDEX NYA A: "+Paisal.indexOf(2.3F));
-		System.out.println("Size: "+Paisal.size());
+		System.out.println("INDEX NYA A: "+kur.indexOf(2.3F));
+		System.out.println("Size: "+kur.size());
 }
 
 }
