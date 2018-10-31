f4e87c7ef561a8702c1a57465c2f09d44624b0fa
Kurniadinurmiati <43645904+Kurniadinurmiati@users.noreply.github.com>
2018-10-31 07:16:41 -0700

    Create ArrayListFloat.java

---
+++ ArrayListFloat.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+public class ArrayListFloat {
+	//Nama : Paisal
+	//Kelas : TeknikInformatikaE 17
+	public static void main(Float[] args) {
+		ArrayList<Float> Paisal = new ArrayList<Float>();
+		Paisal.add(7.3F);
+		Paisal.add(9.5F);
+		Paisal.add(2.9F);
+		Paisal.add(5.9F);
+		System.out.println(Paisal.toString());
+		System.out.println("INDEX NYA A: "+Paisal.indexOf(2.3F));
+		System.out.println("Size: "+Paisal.size());
+}
+
+}
+
+
