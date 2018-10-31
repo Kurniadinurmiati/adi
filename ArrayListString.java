2bc767cfab8f3708a92f0f0584a2382ce1557953
Kurniadinurmiati <43645904+Kurniadinurmiati@users.noreply.github.com>
2018-10-31 07:10:25 -0700

    Create ArrayListString.java

---
+++ ArrayListString.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class ArrayListString {
+	//Nama : kur
+	//Kelas : TeknikInformatikaE 17
+	public static void main(String[] args) {
+			ArrayList<String> kur = new ArrayList<String>();
+			kur.add("alfandhy");
+			kur.add("TEKNIK");
+			kur.add("Informatika");
+			System.out.println(kur.toString());
+			System.out.println("INDEX NYA A: "+kur.indexOf("A"));
+			System.out.println("Size: "+kur.size());
+	}
+
+}
